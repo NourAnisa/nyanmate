fcd2781db25a9e8422ad3ba8cbaf56640d3f9edc
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2026-09-14 07:56:05 +0800

    feat: add NyanMate system tray controls

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use tauri::{
+    menu::{Menu, MenuItem},
+    tray::TrayIconBuilder,
+    Manager,
+};
+
 #[tauri::command]
 fn companion_status() -> &'static str {
     "NyanMate is awake"
@@ -6,6 +12,37 @@ fn companion_status() -> &'static str {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let show = MenuItem::with_id(app, "show", "Show NyanMate", true, None::<&str>)?;
+            let hide = MenuItem::with_id(app, "hide", "Hide NyanMate", true, None::<&str>)?;
+            let quit = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+            let menu = Menu::with_items(app, &[&show, &hide, &quit])?;
+
+            let mut tray = TrayIconBuilder::new()
+                .tooltip("NyanMate")
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id.as_ref() {
+                    "show" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.show();
+                            let _ = window.set_focus();
+                        }
+                    }
+                    "hide" => {
+                        if let Some(window) = app.get_webview_window("main") {
+                            let _ = window.hide();
+                        }
+                    }
+                    "quit" => app.exit(0),
+                    _ => {}
+                });
+
+            if let Some(icon) = app.default_window_icon() {
+                tray = tray.icon(icon.clone());
+            }
+            tray.build(app)?;
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![companion_status])
         .run(tauri::generate_context!())
         .expect("error while running NyanMate");

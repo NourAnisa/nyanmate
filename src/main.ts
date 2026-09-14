2e9b46363ea131a78f1df0a334ac45fdd1de5c04
Nor Anisa <48305341+NourAnisa@users.noreply.github.com>
2026-09-14 08:20:39 +0800

    style: load v0.8 choreography styles

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import './presentation.css'
 import './v05.css'
 import './v06.css'
 import './v07.css'
+import './v08.css'
 
 const Root = getCurrentWindow().label === 'presenter' ? PresenterView : App
 createApp(Root).mount('#app')
